21ec23a23928bdd003a595f48d934ea6f36a1719
Julian1729 <hernandez.julian17@gmail.com>
2018-04-12 13:30:18 -0400

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,27 @@ var mongoose = require('mongoose');
 
 // setup to use promises
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+//check if we are in Heroku Enviornment
+if(process.env.PORT){
+  console.log('Inside Heroku');
+  // http://mlab.com password
+  var username = 'theapp';
+  var password = 'j1102hJBJ';
+
+  var uri = `mongodb://${username}:${password}@ds149373.mlab.com:49373/todoappnodecourse`
+  mongoose.connect(uri);
+}else{
+  // we are inside localhost
+  console.log('In Local Mode');
+  mongoose.connect('mongodb://localhost:27017/TodoApp');
+}
+
+//mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost:27017/TodoApp');
+
+// mongodb://julian1729>:<dbpassword>@ds149373.mlab.com:49373/todoappnodecourse
+
+
 
 module.exports = {
   mongoose
